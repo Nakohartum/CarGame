de4772c8b28739c55559020b5f219434cd1b6dee
Nakohartum <artemofgta@gmail.com>
2022-03-10 18:14:29 +0500

    Домашнее задание 4
    Отрефакторил

---
+++ Assets/_Root/Scripts/GameData.cs
@@ -0,0 +1,15 @@
+﻿using Game;
+using Profile;
+using Services;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = nameof(GameData), menuName = "Configs/"+nameof(GameData))]
+internal class GameData : ScriptableObject
+{
+    [field: SerializeField] public float SpeedCar {get; private set;}
+    [field: SerializeField] public float JumpPower {get; private set;}
+    [field: SerializeField] public GameState InitialState {get; private set;}
+    [field: SerializeField] public TransportType TransportType {get; private set;}
+    [field: SerializeField] public Settings Settings {get; private set;}
+    [field: SerializeField] public ProductLibrary ProductLibrary {get; private set;}
+}
