f3af70541f2497cef1d9f6050a94797d110a0cfe
Nakohartum <artemofgta@gmail.com>
2022-04-16 17:10:01 +0500

    Added bundles

---
+++ Assets/_Root/Scripts/BundleExamples/AssetBundleViewBase.cs
@@ -0,0 +1,7 @@
+﻿namespace AssetBundles
+{
+    public class AssetBundleViewBase
+    {
+        
+    }
+}
---
+++ Assets/_Root/Scripts/BundleExamples/DataSpriteBundle.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+
+namespace AssetBundles
+{
+    [Serializable]
+    internal class DataBundleAsset : IDataBundle
+    {
+        public string Name { get; set; }
+        public Sprite Icon { get; set; }
+    }
+}
